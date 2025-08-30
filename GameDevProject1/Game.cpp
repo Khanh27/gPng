32fa3eb76f7879f701912623f16f1dbb7c44e9f9
Khanh27 <tribuitruong@gmail.com>
2025-08-30 12:59:56 -0600

    implement escape function to close game window

--- GameDevProject1/Game.cpp
+++ GameDevProject1/Game.cpp
@@ -48,7 +48,20 @@ void Game::runGameLoop() {
 }
 
 void Game::processInputs() {
+	SDL_Event event; 
 
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+		}
+	}
+
+	const Uint8* state = SDL_GetKeyboardState(NULL);
+	if (state[SDL_SCANCODE_ESCAPE]) {
+		isRunning = false;
+	}
 }
 void Game::updateGame() {
 
