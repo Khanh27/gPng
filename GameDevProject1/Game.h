ff070eb47974c0d66bfe49d5a2a3fbb57e8df210
Khanh27 <tribuitruong@gmail.com>
2025-08-29 19:24:14 -0600

    Add project files.

---
+++ GameDevProject1/Game.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SDL2/SDL.h>
+
+
+class Game {
+	public:
+		Game();
+
+		bool Initialize();
+
+		void runLoop();
+
+		void shutDown();
+
+	private:
+		void processInput();
+		void updateGame();
+		void GenerateOutput();
+
+		SDL_Window* mWindow;
+		bool mIsRunning
+};
